749adb04a3247374bb80b84ff5745969edc8f538
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2014-12-09 21:54:00 +0100

    Removing more vendors

--- frontend/vendor/angular-markdown-filter/markdown.js
+++
@@ -1,9 +0,0 @@
-angular.module('markdown', [])
-    .filter('markdown', function () {
-        var converter = new Showdown.converter();
-        return function (text) {
-            var markdown = text || '';
-            var html = converter.makeHtml(markdown);
-            return html;
-        };
-    });
