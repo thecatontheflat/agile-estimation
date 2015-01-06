37c424f43abdf4c8e8a627c288cf3db19fc4d7f9
Vitaliy Zurian <vitaliy.zurian@gmail.com>
2015-01-06 21:39:31 +0100

    Refactoring

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var packageInfo = require('./package.json');
+
 try {
     var config = require('./config.json');
 } catch (err) {
@@ -14,16 +15,14 @@ var http = require('http');
 var path = require('path');
 var passport = require('passport');
 var UserModel = require('./models/user').User;
-var GameModel = require('./models/game').Game;
 var helpers = require('./helpers/helpers');
-var cookie = require('cookie');
+var socketRoutes = require('./routes/sockets');
 
 var environment = (process.argv[2] || 'prod');
 var app = express();
 
 // Database connection
 var mongoose = require('mongoose', {server: {auto_reconnect: true}});
-console.log('Connecting to ' + process.env.MONGOSOUP_URL);
 mongoose.connect(process.env.MONGOSOUP_URL || config.database.path);
 
 // Session storage, using MongoDB
@@ -96,113 +95,8 @@ app.get('/game/api/players/remove/:id', gameController.removeGamePlayersApi);
 
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
+socketRoutes(io);
 
-io.sockets.on('connection', function (client) {
-    var clientCookies = client.handshake.headers.cookie;
-    var clientSessionCookie = cookie.parse(clientCookies)['express.sid'];
-
-
-    client.on('disconnect', function () {
-        var condition = {_id: client['room']};
-        GameModel.findOne(condition, function (err, game) {
-            if (!game || game === null) {
-                return;
-            }
-
-            for (var i = 0; i < game.players.length; i++) {
-                if (game.players[i].session == clientSessionCookie) {
-                    game.players[i].isConnected = false;
-                }
-            }
-            game.markModified('players');
-
-            game.save();
-            client.broadcast.emit('updateGameField', game);
-        });
-    });
-
-    // Game Play sockets
-    client.on('joinGame', function (emitData) {
-        client['room'] = emitData.gameId;
-        client['name'] = emitData.userName;
-
-        client.join(emitData.gameId);
-
-        var condition = {_id: emitData.gameId};
-        GameModel.findOne(condition, function (err, game) {
-            if (!game) {
-                return;
-            }
-
-            // If user exists in session
-            if (emitData.isPlayerExists) {
-                var players = game.players;
-
-                for (var i = 0; i < players.length; i++) {
-                    if (players[i].session == clientSessionCookie) {
-                        players[i].isConnected = true;
-                        players[i].avatar = emitData.avatar;
-                        break;
-                    }
-                }
-                game.players = players;
-            } else {
-                game.players.push({
-                    name: emitData.userName,
-                    isConnected: true,
-                    session: clientSessionCookie,
-                    avatar: emitData.avatar
-                });
-            }
-
-            game.markModified('players');
-            game.save(function (error, game) {
-            });
-
-//            io.sockets.emit('updateGameField', game);
-            io.sockets.emit('joinGame', game);
-        });
-    });
-
-    client.on('updateGameField', function (data) {
-        if (data.creator == "vitaliy.zurian@gmail.com" && data.name == "Demo Game") {
-            return;
-        }
-
-        var condition = {_id: data._id};
-        GameModel.findOne(condition, function (err, game) {
-            game.cardsRow = helpers.cleanArray(data.cardsRow);
-            game.cardsDeck = helpers.cleanArray(data.cardsDeck);
-            game.resizableColumns = data.resizableColumns;
-            game.state = data.state;
-            game.save();
-        });
-
-        client.broadcast.to(client['room']).emit('updateGameField', data);
-    });
-
-    client.on('updateResizableColumns', function (data) {
-        client.broadcast.to(client['room']).emit('updateResizableColumns', data);
-    });
-
-    client.on('blinkCard', function (cardIndex) {
-        client.broadcast.to(client['room']).emit('blinkCard', cardIndex);
-    });
-
-    client.on('updateLoggerStack', function (loggerItem) {
-        io.sockets.emit('updateLoggerStack', loggerItem);
-    });
-
-    client.on('busy-ui', function () {
-        client.broadcast.to(client['room']).emit('busy-ui');
-    });
-
-    client.on('free-ui', function () {
-        client.broadcast.to(client['room']).emit('free-ui');
-    });
-});
-
-// Run server
 server.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
